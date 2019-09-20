947cf83eacd155f743f5aec8be124cc6f23bf726
David <dwarrenfive@gmail.com>
2019-09-19 18:00:36 -0700

    added header, links and meta data

--- src/app.jsx
+++ src/app.jsx
@@ -3,7 +3,12 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <div className='App'></div>
+      <div className="container">
+        <div className="card rounded-lg shadow-lg">
+          <h1>San Diego Top Spots</h1>
+          <p>A list of the top 30 spots to see in San Diego, Califonia.</p>
+        </div>
+      </div>
     );
   }
 }
